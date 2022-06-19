4b55061c6ef29f1d1f10cef31cc63104cd8c74fa
Nicolas Heguaburu <nicoheguaburu@gmail.com>
2022-06-19 19:50:04 -0300

    terminado

--- JS/app.js
+++ JS/app.js
@@ -12,32 +12,27 @@ listaProductos.push(new Productos("cuadro", "600", "blanco"));
 listaProductos.push(new Productos("bandeja", "200", "gris"));
 listaProductos.push(new Productos("centro de mesa", "850", "marron"));
 
+buscar();
 
 function buscar() {
     let filtrado = prompt("Filtra por nombre, precio o color");
     if (filtrado == "nombre") {
+        let busqueda = prompt("busca entre mesita, cuadro, bandeja y centro de mesa y ve los resultados por consola");
         let arregloNombres = listaProductos.filter(function(objeto) {
-            let busqueda = prompt("busca entre mesita, cuadro, bandeja y centro de mesa y ve los resultados por consola");
             return (objeto.articulo == busqueda);
         })
         console.log(arregloNombres);
-
-    } else if (filtrado == "precio"); {
+    } else if (filtrado == "precio") {
+        let busqueda = prompt("busca por los siguientes precios 200, 600, 850 2000");
         let arregloPrecios = listaProductos.filter(function(objeto) {
-            let busqueda = prompt("busca por los siguientes precios 200, 600, 850 2000");
             return (objeto.precio == busqueda);
         })
         console.log(arregloPrecios);
-
-    } else if (filtrado == "color"); {
+    } else if (filtrado == "color") {
+        let busqueda = prompt("busca por los colores celeste, blanco, gris y marron");
         let arregloColor = listaProductos.filter(function(objeto) {
-            let busqueda = prompt("busca por los colores celeste, blanco, gris y marron");
             return (objeto.color == busqueda);
         })
         console.log(arregloColor);
-    } else {
-        buscar();
     }
 }
-
-buscar();
